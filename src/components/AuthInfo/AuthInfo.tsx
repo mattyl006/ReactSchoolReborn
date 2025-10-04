ca2af11dfd3da69754788e204dc132c7e4f0a19c
mattyl006 <wirus006@gmail.com>
2025-10-04 11:50:06 +0200

    apply AuthContext.Provider to have access for actual context values

--- src/components/AuthInfo/AuthInfo.tsx
+++ src/components/AuthInfo/AuthInfo.tsx
@@ -3,15 +3,18 @@ import AuthCredentials from "../AuthCredentials";
 import Header from "../Header";
 import { HeaderType } from "../Header/enums/HeaderType";
 import { cn } from "../../utils/cn";
+import { AuthContext } from "../../contexts/AuthContext";
 
 type AuthInfoProps = { className?: string };
 
 const AuthInfo: React.FC<AuthInfoProps> = (props) => {
   return (
-    <div className={cn(props.className, "flex flex-col gap-4")}>
-      <Header type={HeaderType.H2}>Auth Info</Header>
-      <AuthCredentials />
-    </div>
+    <AuthContext.Provider value={{ isLogged: false }}>
+      <div className={cn(props.className, "flex flex-col gap-4")}>
+        <Header type={HeaderType.H2}>Auth Info</Header>
+        <AuthCredentials />
+      </div>
+    </AuthContext.Provider>
   );
 };
 
