a35367007ed294e1e68208e01b4369af000dd1d8
mattyl006 <wirus006@gmail.com>
2025-10-26 19:21:48 +0100

    switch theme colors and icon

--- src/components/AuthInfo/components/ThemeSwitcher/ThemeSwitcher.tsx
+++ src/components/AuthInfo/components/ThemeSwitcher/ThemeSwitcher.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import useThemeContext from "../../../../hooks/useThemeContext";
 import Button from "../../../Button";
 import { SunIcon, MoonIcon } from "@heroicons/react/24/solid";
@@ -7,11 +7,19 @@ import { ThemeEnum } from "../../../../contexts/ThemeContext/ThemeContext";
 type ThemeSwitcherProps = { className?: string };
 
 const ThemeSwitcher: React.FC<ThemeSwitcherProps> = (props) => {
-  const { toggle, theme } = useThemeContext();
+  const [theme, setTheme] = useState(ThemeEnum.LIGHT);
+  const { toggle } = useThemeContext();
+
+  const handleClick = () => {
+    toggle();
+    setTheme((theme) =>
+      theme === ThemeEnum.LIGHT ? ThemeEnum.DARK : ThemeEnum.LIGHT
+    );
+  };
 
   return (
-    <Button onClick={toggle} className={props.className}>
-      {theme.current === ThemeEnum.LIGHT ? (
+    <Button onClick={handleClick} className={props.className}>
+      {theme === ThemeEnum.LIGHT ? (
         <MoonIcon className="w-6 h-6" />
       ) : (
         <SunIcon className="w-6 h-6" />
